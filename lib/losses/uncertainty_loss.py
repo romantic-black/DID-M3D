90708c4873cb91a500e97a93af16279cc54b8169
root <1459282203@qq.com>
2023-12-05 21:30:58 +0800

    change: no uncertain

--- lib/losses/uncertainty_loss.py
+++ lib/losses/uncertainty_loss.py
@@ -8,7 +8,9 @@ def laplacian_aleatoric_uncertainty_loss(input, target, log_variance, reduction=
         Geometry and Uncertainty in Deep Learning for Computer Vision, University of Cambridge
     '''
     assert reduction in ['mean', 'sum']
-    loss = 1.4142 * torch.exp(-0.5*log_variance) * torch.abs(input - target) + 0.5*log_variance
+    # loss = 1.4142 * torch.exp(-0.5*log_variance) * torch.abs(input - target) + 0.5*log_variance
+    loss = torch.abs(input - target)
+
     return loss.mean() if reduction == 'mean' else loss.sum()
 
 def gaussian_aleatoric_uncertainty_loss(input, target, log_variance, reduction='mean'):
